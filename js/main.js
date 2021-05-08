ded4dced4695a93ca93486a797a2421c5d489f53
Naim Ahmed <na2359034@gmail.com>
2021-05-08 12:58:09 +0600

    animate change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const cursor = document.querySelector(".cursor");
+const navLinks = document.querySelectorAll(".nav-item a");
+
+window.addEventListener('mousemove',cursorMove);
+
+function cursorMove(e){
+    cursor.style.top = e.pageY + 'px';
+    cursor.style.left = e.pageX + 'px';
+}
+
+navLinks.forEach(link => {
+    link.addEventListener('mousemove',() => {
+        cursor.classList.add('link-grow');
+        link.classList.add('hovered-link');
+    })
+
+    link.addEventListener('mouseleave',() => {
+        cursor.classList.remove('link-grow');
+        link.classList.remove('hovered-link');
+    })
+})
 
 new WOW().init();
 
