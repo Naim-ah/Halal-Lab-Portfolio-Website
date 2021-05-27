fe7491491c7d0b58d2b6ad7140298b3214d5bcbd
Naim Ahmed <na2359034@gmail.com>
2021-05-27 10:40:26 +0600

    arrow animate change

--- js/main.js
+++ js/main.js
@@ -1,88 +1,4 @@
 
-// custom cursor======
-// const cursor = document.querySelector(".cursor");
-// const cursor2 = document.querySelector(".cursor2");
-
-// const navLink = document.querySelectorAll(".nav-item a");
-// const mainContent = document.querySelectorAll(".main-content h1")
-
-// document.addEventListener("mousemove",function(e){
-//     cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
-// })
-
-// navLink.forEach(link => {
-//     link.addEventListener('mousemove',() => {
-//                 cursor.classList.add('link-grow');
-//                 link.classList.add('hovered-link');
-//             })
-        
-//             link.addEventListener('mouseleave',() => {
-//                 cursor.classList.remove('link-grow');
-//                 link.classList.remove('hovered-link');
-//             })
-// })
-
-// mainContent.forEach(content => {
-//     content.addEventListener('mousemove',() => {
-//                 cursor.classList.add('content-grow');
-//                 content.classList.add('hovered-content');
-//             })
-        
-//             content.addEventListener('mouseleave',() => {
-//                 cursor.classList.remove('content-grow');
-//                 content.classList.remove('hovered-content');
-//             })
-// })
-
-
-// intersectionObserver =========
-
-// window.addEventListener('DOMContentLoaded', setup); 
-
-// function setup() {
-//     const options = {
-//         rootMargin: '0px 0px -200px 0px'
-//     }
-
-//     const observer = new IntersectionObserver((entries, observer) => {
-//         entries.forEach(entry => {
-//             if(entry.isIntersecting) {
-//                 entry.target.classList.add('show');
-//                 observer.unobserve(entry.target);
-//             } else {
-//                 return;
-//             }
-//         })
-//     }, options);
-
-    // const h1 = document.querySelector('.scroll');
-    // observer.observe(h1);
-    // const scroll2 = document.querySelector('.scroll2');
-    // observer.observe(scroll2);
-    // const scroll3 = document.querySelector('.scroll3');
-    // observer.observe(scroll3);
-    // const scroll4 = document.querySelector('.scroll4');
-    // observer.observe(scroll4);
-
-    // const mainImg = document.querySelector('.main-image');
-    // observer.observe(mainImg);
-
-    // const processScroll = document.querySelectorAll('.process-scroll');
-    // processScroll.forEach(processScroll => observer.observe(processScroll));
-
-    // const sectionContent = document.querySelectorAll('.section-content');
-    // sectionContent.forEach(secContentItem => observer.observe(secContentItem));
-
-    // const projectItemTitle = document.querySelectorAll('.project-item-title');
-    // projectItemTitle.forEach(item => observer.observe(item));
-
-    // const projectItem = document.querySelectorAll('.scroll-project-animate');
-    // projectItem.forEach(projectImg => observer.observe(projectImg));
-
-    // const processItem = document.querySelectorAll('.process-scroll');
-    // processItem.forEach(process => observer.observe(process));
-
-// }
 
 // wow js=======
 // new WOW().init();
@@ -91,6 +7,25 @@
 window.addEventListener('load',function(){
     document.querySelector('body').classList.add("loaded")  
   });
+
+
+// process item
+// const process = document.querySelector(".process-item");
+
+// process.addEventListener("mouseover",() => {
+//     const smText = document.querySelector(".process-sm-text");
+//     smText.classList.add("show")
+// })
+
+// process.addEventListener("mouseout",() => {
+//     const smText = document.querySelector(".process-sm-text");
+//     smText.classList.remove("show")
+// })
+
+
+
+
+
 
 // owl carousel=======
 $('.clients-carousel').owlCarousel({
